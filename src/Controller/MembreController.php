4862c79cf501bd91ad94ab86f566b058ee99d562
ronicairo <roni.cairo@outlook.fr>
2023-04-20 10:26:56 +0200

    update membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -44,6 +45,8 @@ class MembreController extends AbstractController
             $membre->setCreatedAt(new DateTime());
             $membre->setUpdatedAt(new DateTime());
 
+            $membre->setPassword($passwordHasher->hashPassword($membre, $membre->getPassword()));
+
             $membre->setRoles(['ROLE_ADMIN']);
 
 
@@ -69,5 +72,48 @@ class MembreController extends AbstractController
         ]);
     }
 
+    // ------------------------------ HARD-DELETE-ARTICLE -------------------------------
+#[Route('/supprimer-un-membre/{id}', name: 'soft_delete_membre', methods: ['GET'])]
+public function softDeleteMembre(Membre $membre, MembreRepository $repository): Response
+
+{
+
+    $membre->setDeletedAt(new DateTime());
+
+    $repository->remove($membre, true);
+
+    $this->addFlash('success', "Le membre a bien été supprimé de la base.");
+    // return $this->render('admin/register_form.html.twig');
+    return $this->redirectToRoute('register');
+
+} // end hardDeleteArticle()
+
+#[Route('/modifier-un-membre{id}', name: 'update_membre', methods: ['GET', 'POST'])]
+    public function updateChambre(Membre $membre, Request $request, MembreRepository $repository, SluggerInterface $slugger,EntityManagerInterface $entityManager): Response
+    {
+
+        $form = $this->createForm(MembreFormType::class, $membre)
+            ->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $membre->setCreatedAt(new DateTime());
+            $membre->setUpdatedAt(new DateTime());
+        
+
+            // $membre->setAuthor($this->getUser());
+
+            $repository->save($membre, true);
+
+            $this->addFlash('success', "L'utilisateur a bien eté modifié avec succès !");
+            // return $this->redirectToRoute(('show_dashboard'));
+        }
+        $membres=$entityManager->getRepository(Membre::class)->findAll();
+        return $this->render('admin/register_form.html.twig', [
+            'form' => $form->createView(),
+            'membres' => $membres
+
+        ]);
+    }
 
 }
